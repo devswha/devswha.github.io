9d37462743c730078615b671410641f15f095a9b
swhaKo <swha@unist.ac.kr>
2019-07-22 09:29:54 +0900

    test

---
+++ _pages/resource-archive.md
@@ -0,0 +1,6 @@
+---
+title: "Posts by Category"
+layout: resource
+permalink: /resource/
+author_profile: true
+---
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Project Homepage Open!",
+        "excerpt":" ","categories": ["Resource"],
+        "tags": [],
+        "url": "http://localhost:4000/swha/resource/test/",
+        "teaser":null}]
