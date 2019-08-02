c74a643a3875acd177a024114bc8f36cd16d6034
swhaKo <swha@unist.ac.kr>
2019-08-02 16:55:34 +0900

    test3

---
+++ _resources/Scenario_Generator.md
@@ -0,0 +1,6 @@
+---
+title: "Scenario Generator"
+permalink: /resources/Scenario_Generator/
+excerpt: "Scenario Generator"
+toc: true
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,6 +4,21 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/swha/resource/test/",
         "teaser":null},{
+        "title": "RoboCup Rescue Simulation with Deep Learning",
+        "excerpt":"Disasters such as earthquake and tsunami can cause significant destruction to a city and hurt many people. To reduce the amount of the dead troll, fast disaster response to rescue survivors in a disaster zone is of paramount importance. However, the problem is all the current method to manage disaster...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/swha/resources/Intro/",
+        "teaser":null},{
+        "title": "RoboCup Rescue Simulation (RCRS)",
+        "excerpt":"The objective of our study is to develop software packages to predict the location of the injured people using deep learning based on virtual disaster simulation. To utilize deep learning techniques mainly, as many data sets as possible are needed to train the machine learning model. However, the data sets...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/swha/resources/RCRS/",
+        "teaser":null},{
+        "title": "Scenario Generator",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/swha/resources/Scenario_Generator/",
+        "teaser":null},{
         "title": "Resources",
         "excerpt":" ","categories": [],
         "tags": [],
